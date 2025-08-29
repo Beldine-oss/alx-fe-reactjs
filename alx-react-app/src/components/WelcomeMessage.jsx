ccb2b9ada36ca74fd39a26a5ecf78691fa18302f
Beldine Vera <verahbeldine@gmail.com>
2025-08-29 20:48:34 +0300

    Correcting JSX message

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -1,9 +1,10 @@
 function WelcomeMessage() {
-  return (
-    <div>
-      <p>This is a simple JSX component.</p>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Welcome to ALX React APP!</h1>
+            <p>This is a simple JSX component.</p>
+        </div>
+    );
 }
 
 export default WelcomeMessage;
