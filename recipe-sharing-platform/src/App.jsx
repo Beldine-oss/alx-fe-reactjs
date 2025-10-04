96a4feee360d2b337e306d7433aa15af4c221221
Beldine Vera <verahbeldine@gmail.com>
2025-10-04 17:06:28 +0300

    Add HomePage component with mock recipe data and responsive grid layout

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,9 +1,11 @@
+import React from "react";
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold text-blue-500">
-      Hello Recipe Sharing Platform!
-    </h1>
+    <div className="bg-gray-100 min-h-screen">
+      <HomePage />
+    </div>
   );
 }
 
